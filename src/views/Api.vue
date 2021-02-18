0a96a768bdbb1030b2425704adc0e5eedcca5a5f
pgmez92 <pgmez92@gmail.com>
2021-02-18 14:10:53 +0100

    implementando un mapa con leaflet

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Api">
     <div id="mapid"></div>
-    <button @click="marca">botonsito</button>
+    <button class="btn-success" @click="marca">butonsito</button>
   </div>
 </template>
 
@@ -15,8 +15,8 @@ export default {
   setup() {
     let mymap;
     onMounted(() => {
-      L.map("mapid").setView([51.505, -0.09], 13);
-      mymap = L.tileLayer(
+      mymap = L.map("mapid").setView([41.596549, -4.751587], 10);
+      L.tileLayer(
         "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
         {
           maxZoom: 20,
@@ -24,15 +24,37 @@ export default {
             '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         }
       ).addTo(mymap);
+      mymap.on("click", ver);
     });
+    const marca = () => {
+      let marker = L.marker([36.8165, 10.2172]).addTo(mymap);
+      var circle = L.circle([36.8165, 10.2172], {
+        color: "red",
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 500,
+      }).addTo(mymap);
+    };
 
-    return { mymap };
+    const ver = (e) => {
+      let popup = L.popup();
+      popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+    };
+
+    return { mymap, marca, ver };
   },
 };
 </script>
 
 <style scoped>
 #mapid {
-  height: 500px;
+  height: 400px;
+}
+.btn-success {
+  margin: 10px;
+  border-radius: 10%;
 }
 </style>
